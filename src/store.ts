142cd00240a2b5cae6652597ba94006eae49db03
Lakshya Gupta <lakshya1176@gmail.com>
2026-03-05 01:20:24 +0530

    tutorial build

---
+++ src/store.ts
@@ -0,0 +1,25 @@
+import { createStore, type Action } from "redux";
+
+export type State = {
+    sadCount: number;
+    happyCount: number;
+}
+
+const initialState: State = {
+    sadCount: 0,
+    happyCount: 0
+}
+
+const reducer = (currentState: State = initialState, action: Action): State => {
+    if (action.type === "happy button clicked") {
+        return { ...currentState, happyCount: currentState.happyCount + 1 };
+    } else if (action.type === "sad button clicked") {
+        return { ...currentState, sadCount: currentState.sadCount + 1 };
+    } else {
+        return currentState;
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const store = createStore(reducer, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()); //createStore is deprecated
+export default store;
